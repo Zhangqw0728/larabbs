18127148ae442a5abb211a2c88feb4e84def3405
zhangqw <30414354+Zhangqw0728@users.noreply.github.com>
2021-03-29 10:27:05 +0000

    后台权限

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,6 +3,6 @@
         <p class="float-left">
             由 <a href="#" target="_blank">Sumer</a>设计和编码
         </p>
-        <p class="float-right"><a href="">联系我们</a></p>
+        <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
